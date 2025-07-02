4870a6a72368d5be16ee0f4d5bb66a67defb5749
Sanmathi-Sedhupathi <sanmathisedhupathi2004@gmail.com>
2025-07-02 15:36:18 +0530

    added render url

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,17 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:8081/api/",
+  baseURL: "https://ai-image-generator-mern-backend.onrender.com/api/",
+ 
 });
 
+// Fetch all posts
 export const GetPosts = async () => await API.get("/post");
+
+// Create a new post
 export const CreatePost = async (data) => await API.post("/post", data);
 
-
+// Generate image from prompt
 export const GenerateImageFromPrompt = async (data) => {
- 
   return await API.post("/generate-image/", data);
 };
